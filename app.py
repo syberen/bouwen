f27983b3e3bdb516077f7c26ee956e40705a7dd4
syberen <syberen.van.munster@gmail.com>
2022-02-07 10:41:51 +0100

    Cache aggregate result in memory

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask
 
+from flask_caching import Cache
+
 from services import CBSAggregateService
 
+four_hours = 60 * 60 * 4
+config = {
+    "CACHE_TYPE": "SimpleCache",
+    "CACHE_DEFAULT_TIMEOUT": four_hours,
+}
 app = Flask(__name__)
+app.config.from_mapping(config)
+
+cache = Cache(app)
 
 
 @app.route("/aggregate")
+@cache.cached()
 def aggregate():
     aggregate_service = CBSAggregateService()
     data = aggregate_service.get_aggregate_data()
